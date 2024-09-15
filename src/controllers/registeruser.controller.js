b2f046a84cebef01ae45392b7edf2a8d5101f5e8
mandip kamaliya <mandipkamaliya7@gmail.com>
2024-09-15 22:52:57 +0530

    add user login function

--- src/controllers/registeruser.controller.js
+++ src/controllers/registeruser.controller.js
@@ -3,8 +3,25 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {uploadoncloudinary} from "../utils/cloudinary.js"
 import {User} from "../models/user.models.js"
+import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
 
-
+const GenerateAccessandRefreshtokens=asyncHandler(async(userid)=>{
+     try {
+      const user=await User.findById(userid)
+      if(!user){
+        throw new ApiError(400,"user not found")
+      }
+       const Accesstoken=user.generateAccessToken()
+       const refreshtoken=user.generateRefreshToken()
+       user.refreshtoken=refreshtoken
+       await  user.save({validateBeforeSave:false})
+       return {Accesstoken,refreshtoken}
+      
+     } catch (error) {
+        throw new ApiError(500,"something went wrong while generating tokens")
+     }  
+})
 const registeruser=asyncHandler( async(req,res)=>{
     const {username,email,fullname,password}=req.body
 if([username,email,fullname,password].some((field)=>field?.trim()=="")){
@@ -53,5 +70,36 @@ if([username,email,fullname,password].some((field)=>field?.trim()=="")){
   return res.status(201).json(
      new ApiResponse(200,createduser,"user created successfully")
     )
+})
+const loginuser=asyncHandler(async(req,res)=>{
+   const {username,email,password}=req.body
+   if(!username && !email){
+    throw new ApiError(400," usename or email is required")
+   }
+   const user=User.findOne({
+    $or:[{username},{email}]
+   })
+  const ispasswordvalid= await user.isPassword(password)
+  if(!ispasswordvalid){
+    throw new ApiError(400,"Invalid credientials")
+  }
+  const {Accesstoken,refreshtoken}=await generateAccessToken(user._id)
+  const loggedinuser=await User.findById(user._id).select("-password -refreshtken")
+  if(!loggedinuser){
+    throw new ApiError(404,"something went wrogn while login")
+  }
+  const options={
+    httpOnly:true,
+    secure:process.env.NODE_ENV==="production"
+  }
+  return res
+  .status(200)
+  .cookie("Accesstoken",Accesstoken,options)
+  .cookie("refreshtoken",refreshtoken,options)
+  .json( new ApiResponse(
+    200,
+    {user:loggedinuser,Accesstoken,refreshtoken},
+    "user logged in successfully"
+  ))
 })
 export {registeruser}
